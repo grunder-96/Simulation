11767bf0d2a7a0c54bf4db9a283156df25e095ac
Grigory Taran <grunder-96@mail.ru>
2024-09-01 21:30:12 +0300

    add search surrounding coordinates method in GameMap

--- src/simulation/map/GameMap.java
+++ src/simulation/map/GameMap.java
@@ -22,4 +22,28 @@ public class GameMap {
     public Optional<Entity> getEntity(Coordinate coordinate) {
         return Optional.ofNullable(cells.get(coordinate));
     }
+
+    public List<Coordinate> getSurroundingCoordinates(Coordinate coordinate) {
+        List<Coordinate> surroundingCoordinates = new ArrayList<>();
+        int x = coordinate.getX();
+        int y = coordinate.getY();
+
+        for (int i = x - 1; i <= x + 1; i++) {
+            if (!isWithinMapBounds(i)) {
+                continue;
+            }
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (!isWithinMapBounds(j) || (x == i && y == j)) {
+                    continue;
+                }
+                Coordinate adjacentCoordinate = new Coordinate(i, j);
+                surroundingCoordinates.add(adjacentCoordinate);
+            }
+        }
+        return surroundingCoordinates;
+    }
+
+    private boolean isWithinMapBounds(int i) {
+        return i >= GameMap.START_COORDINATE && i <= GameMap.MAP_SIZE;
+    }
 }
