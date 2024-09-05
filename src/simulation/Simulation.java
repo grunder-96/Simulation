c1c3b8a3df6041c3a211d7ab97fded5b531dab61
Grigory Taran <grunder-96@mail.ru>
2024-09-05 11:11:59 +0300

    feature: add Simulation.class

---
+++ src/simulation/Simulation.java
@@ -0,0 +1,43 @@
+package simulation;
+
+import simulation.action.Action;
+import simulation.action.init.PopulateMapAction;
+import simulation.action.turn.MoveCreaturesAction;
+import simulation.entity.creature.Herbivore;
+import simulation.map.GameMap;
+
+public class Simulation {
+
+    GameMap map = new GameMap();
+    TargetSearcher searcher = new TargetSearcher(map);
+    private Renderer renderer = new Renderer(map);
+    private boolean isSimulationOver;
+    private int simulationStepCounter;
+
+    private Action populateMapAction = new PopulateMapAction(map, searcher);
+    private Action moveCreaturesAction = new MoveCreaturesAction(map, searcher);
+
+    public void startSimulation() {
+        populateMapAction.doAction();
+        while (!isSimulationOver) {
+            nextTurn();
+        }
+    }
+
+    private void nextTurn() {
+        System.out.println("current step: " + simulationStepCounter);
+        renderer.render();
+        moveCreaturesAction.doAction();
+        simulationStepCounter++;
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        updateIsSimulationOver();
+    }
+
+    private void updateIsSimulationOver() {
+        isSimulationOver = map.getCoordinatesByType(Herbivore.class).isEmpty();
+    }
+}
