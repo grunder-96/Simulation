e5a81ecc137218fc0f67bf48800aaf464fb27a4e
Grigory Taran <grunder-96@mail.ru>
2024-09-05 11:16:55 +0300

    add simulation lauch in Main

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -3,6 +3,7 @@ package simulation;
 public class Main {
 
     public static void main(String[] args) {
-
+        Simulation simulation = new Simulation();
+        simulation.startSimulation();
     }
 }
